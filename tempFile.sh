f051b427c8aff97b73ef487c9cade2f39fcc99c1
Kevin Kamto <kamtokevin@gmail.com>
2019-02-08 00:51:34 -0500

    feat: initial script commit

---
+++ tempFile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#Is compatible with shell
+
+rmTemp()
+{
+    echo "Removing 'Temporary' folder"
+    cd ~/Downloads/
+    rm -rf ./temp/
+}
+
+mkTemp()
+{
+    echo "Creating 'Temporary' folder"
+    cd ~/Downloads
+    mkdir ./temp/
+}
+
+
+#Execution zone
+echo "Launching script..."
+rmTemp
+mkTemp
+echo "All Done"
