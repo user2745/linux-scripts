584815141320f9f476c19be1cca67b1639250d9b
Kevin Kamto <kamto@knome.com>
2019-02-20 14:01:47 -0500

    feat: script saves old themes dir

--- themes.sh
+++ themes.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 
+if [ -f ~/.themes/ ]
+#Copies current themes into different folder
+    echo "Themes files detected!"
+    
+    cp -r ~/.themes/ ~/.themes.old/
+    rm -r ~/.themes/
+    echo "Made "
+else 
+    echo "No previous themes dir found.."
+    echo ""
+fi 
 
 #Original Repo
 #https://github.com/tliron/install-gnome-themes.git
